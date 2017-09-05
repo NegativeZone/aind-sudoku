d8c466bf29eb6c5aaff80ccba83f6c1d6673ecb2
NegativeZone <anuroop@bisaria.com>
2017-09-05 22:00:31 +0400

    First solution

--- README.md
+++ README.md
@@ -3,42 +3,8 @@
 
 # Question 1 (Naked Twins)
 Q: How do we use constraint propagation to solve the naked twins problem?  
-A: *Student should provide answer here*
+A: The naked twins heuristic finds two peers with two matching values. Since those values can only exist in either of those two boxes, it eliminates those options from every other mutual peer. This allows for a more thorough elimination process than what is already provided for by the eliminate heuristic. This heuristic is applied over and over again until the puzzle cannot be reduced any further or is solved.
 
 # Question 2 (Diagonal Sudoku)
 Q: How do we use constraint propagation to solve the diagonal sudoku problem?  
-A: *Student should provide answer here*
-
-### Install
-
-This project requires **Python 3**.
-
-We recommend students install [Anaconda](https://www.continuum.io/downloads), a pre-packaged Python distribution that contains all of the necessary libraries and software for this project. 
-Please try using the environment we provided in the Anaconda lesson of the Nanodegree.
-
-##### Optional: Pygame
-
-Optionally, you can also install pygame if you want to see your visualization. If you've followed our instructions for setting up our conda environment, you should be all set.
-
-If not, please see how to download pygame [here](http://www.pygame.org/download.shtml).
-
-### Code
-
-* `solution.py` - You'll fill this in as part of your solution.
-* `solution_test.py` - Do not modify this. You can test your solution by running `python solution_test.py`.
-* `PySudoku.py` - Do not modify this. This is code for visualizing your solution.
-* `visualize.py` - Do not modify this. This is code for visualizing your solution.
-
-### Visualizing
-
-To visualize your solution, please only assign values to the values_dict using the `assign_value` function provided in solution.py
-
-### Submission
-Before submitting your solution to a reviewer, you are required to submit your project to Udacity's Project Assistant, which will provide some initial feedback.  
-
-The setup is simple.  If you have not installed the client tool already, then you may do so with the command `pip install udacity-pa`.  
-
-To submit your code to the project assistant, run `udacity submit` from within the top-level directory of this project.  You will be prompted for a username and password.  If you login using google or facebook, visit [this link](https://project-assistant.udacity.com/auth_tokens/jwt_login) for alternate login instructions.
-
-This process will create a zipfile in your top-level directory named sudoku-<id>.zip.  This is the file that you should submit to the Udacity reviews system.
-
+A: The two diagonals (top left to bottom right, and top right to bottom left) are just an additional unit for the solution to handle. Aside from the definition of the diagonals as additional units, the solution follows the same propagation of the eliminate, naked twins, and only choice constraints (with the depth first search as needed) to reduce the puzzle to the solution.
--- solution.py
+++ solution.py
@@ -1,5 +1,9 @@
 assignments = []
 
+# Values defined from the project intro
+rows = 'ABCDEFGHI'
+cols = '123456789'
+
 def assign_value(values, box, value):
     """
     Please use this function to update your values dictionary!
@@ -27,10 +31,48 @@ def naked_twins(values):
     # Find all instances of naked twins
     # Eliminate the naked twins as possibilities for their peers
 
+    # Find the twins (len=2)
+    twins = [n for n in values.keys() if len(values[n]) == 2]
+    # Find the naked twins (matching values)
+    # TODO remove repetition
+    n_twins_rep = [[a,b] for a in twins for b in twins and peers[a] if values[a] == values[b]]
+    for i in n_twins_rep:
+        # For each pair of twins
+        twin1 = i[0]
+        twin2 = i[1]
+        # Find the common peers
+        peers1 = peers[twin1]
+        peers2 = peers[twin2]
+        peers_int = peers1 & peers2
+        # Erase the common peer values
+        for x in peers_int:
+            if len(values[x])>1:
+                for y in values[twin1]:
+                    values = assign_value(values, x, values[x].replace(y,''))
+    return values
+
+# Helper function from the project intro
 def cross(A, B):
     "Cross product of elements in A and elements in B."
-    pass
+    return [s+t for s in A for t in B]
 
+# Definitions from the project intro
+boxes = cross(rows, cols)
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+
+# Diagonals defined manually
+# TODO automate diagonal generation
+diag_units_a = [['A1','B2','C3','D4','E5','F6','G7','H8','I9']]
+diag_units_b = [['A9','B8','C7','D6','E5','F4','G3','H2','I1']]
+
+# More definitions from the project intro
+unitlist = row_units + column_units + square_units + diag_units_a + diag_units_b
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
+# Helper function defined in the project intro
 def grid_values(grid):
     """
     Convert grid into a dict of {square: char} with '123456789' for empties.
@@ -41,28 +83,86 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    pass
+    values = []
+    all_digits = '123456789'
+    for c in grid:
+        if c == '.':
+            values.append(all_digits)
+        elif c in all_digits:
+            values.append(c)
+    assert len(values) == 81
+    return dict(zip(boxes, values))
 
+# Helper function defined in the project intro
 def display(values):
     """
     Display the values as a 2-D grid.
     Args:
         values(dict): The sudoku in dictionary form
     """
-    pass
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    return
 
+# Eliminate function as defined in the intro
 def eliminate(values):
-    pass
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            replacement = values[peer].replace(digit,'')
+            values = assign_value(values, peer, replacement)
+    return values
 
+# Only choice function as defined in the intro
 def only_choice(values):
-    pass
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values = assign_value(values, dplaces[0], digit)
+    return values
 
+# Modified function from the intro
 def reduce_puzzle(values):
-    pass
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    stalled = False
+    while not stalled:
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        values = eliminate(values)
+        # Incorporated naked twins heuristic
+        values = naked_twins(values)
+        values = only_choice(values)
 
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        stalled = solved_values_before == solved_values_after
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
+
+# Function as defined from project intro
 def search(values):
-    pass
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes):
+        return values ## Solved!
+    # Choose one of the unfilled squares with the fewest possibilities
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    # Now use recursion to solve each one of the resulting sudokus, and if one returns a value (not False), return that answer!
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
+# Triggers the solution process
 def solve(grid):
     """
     Find the solution to a Sudoku grid.
@@ -72,9 +172,13 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
+    values = search(grid_values(grid))
+    return values
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    #diag_sudoku_grid = '.....6....59.....82....8....45........3........6..3.54...325..6..................'
+    #diag_sudoku_grid = '.................................................................................'
     display(solve(diag_sudoku_grid))
 
     try:
